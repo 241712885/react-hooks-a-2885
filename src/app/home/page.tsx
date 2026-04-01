0c30e16cbe0f48d6dd9b2eca535776511a9cc425
241712885 <mirandaimanuekka@gnail.com>
2026-04-01 08:50:51 +0700

    yh

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Game1 from '@/src/components/Game1';
 
 export default function Home() {
     const router = useRouter();
-    const [allowed, setAllowed] = useState(false);
 
     useEffect(() => {
         const isLogin = localStorage.getItem('isLogin');
 
-        if (isLogin !== 'true') {
-            router.replace('/auth/not-authorized');
-        } else {
-            setAllowed(true);
-        }
-    }, [router]);
+        if (isLogin) {
+            router.push('/auth/not-authorized');
+        }         
+    }, []);
 
-    if (!allowed) return null;
-
-    return <Game1 />;
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
+            <h1 className="text-4xl font-bold mb-8 text-primary">Selamat Datang!</h1>
+            <Game1 />
+        </div>
+    );
 }
