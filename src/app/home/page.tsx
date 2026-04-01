a438ded5df828333189c36026d8af4d588ca98c4
241712885 <mirandaimanuekka@gnail.com>
2026-04-01 12:51:41 +0700

    TOLONG

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,19 +6,23 @@ import Game1 from '@/src/components/Game1';
 
 export default function Home() {
     const router = useRouter();
-    const [allowed, setAllowed] = useState(false);
+    // const [allowed, setAllowed] = useState(false);
+    const [isLogin, setIsLogin] = useState<boolean | null>(null);
 
     useEffect(() => {
         const isLogin = localStorage.getItem('isLogin');
 
         if (isLogin === 'true') {
-            setAllowed(true);
+            setIsLogin(true);
         } else {
+            setIsLogin(false);
+            
             router.replace('/auth/not-authorized');
         }
     }, [router]);
 
-    if (!allowed) return null;
+    if (isLogin === null) return null;
+    if (isLogin === false) return null;
 
     return (
         <div className="flex items-center justify-center min-h-screen">
